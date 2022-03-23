69033d26dcc3b9f1a301d0e344808da240058ca2
Beela <bla@senia.dk>
2022-03-23 11:47:08 +0100

    Add project files.

---
+++ ResultPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using ResultPattern;
+
+NotFoundResult<int> test = new NotFoundResult<int>("1", "test", "testing");
+foreach(var str in test.Errors)
+{
+    Console.WriteLine(str); 
+}
+
+NotFoundResult<int> test2 = new NotFoundResult<int>("23");
+foreach(var str in test2.Errors)
+{
+    Console.WriteLine(str);
+}
---
+++ ResultPattern/Result.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultPattern;
+
+public abstract class Result
+{
+    public abstract ResultTypes ResultType { get; }
+    public abstract IEnumerable<string> Errors { get; }
+}
+
+
+public abstract class Result<T> : Result
+{
+    public abstract T Data { get; }
+}
+
+public class OkResult<T> : Result<T>
+{
+    private readonly T _data;
+    public OkResult(T data)
+    {
+        _data = data;
+    }
+    public override ResultTypes ResultType => ResultTypes.Ok;
+    public override IEnumerable<string> Errors => Array.Empty<string>();
+    public override T Data => _data;
+
+}
+
+public class NoContentResult : Result
+{
+    public NoContentResult()
+    {
+
+    }
+    public override ResultTypes ResultType => ResultTypes.NoContent;
+    public override IEnumerable<string> Errors => Array.Empty<string>();
+}
+
+public class NotFoundResult<T> : Result<T>
+{
+    private IEnumerable<string> _errors;
+    public NotFoundResult(params string[] errors)
+    {
+        _errors = errors;
+    }
+
+    public override ResultTypes ResultType => ResultTypes.NotFound;
+    public override IEnumerable<string> Errors => _errors;
+    public override T Data => default(T);
+}
+
+public enum ResultTypes
+{
+    Ok,
+    NoContent,
+    Invalid,
+    NotFound,
+    Unexpected,
+}
